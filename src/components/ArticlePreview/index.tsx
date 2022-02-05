25d5e0ed993b196d6861f8c97dc71bee94f8ff6a
Cyril Troitsky <troickiykirill@gmail.com>
2022-02-05 14:10:26 +0200

    feat: add margins and adaptability

--- src/components/ArticlePreview/index.tsx
+++ src/components/ArticlePreview/index.tsx
@@ -1,6 +1,7 @@
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import React from 'react';
 import { ArticlePreviewType } from '../../types/queryTypes';
+import Text from '../Text';
 import './style.scss';
 
 interface ArticlePreviewProps extends ArticlePreviewType {
@@ -11,6 +12,7 @@ const ArticlePreview: React.FC<ArticlePreviewProps> = ({
   title,
   header,
   className,
+  description,
 }) => {
   const image = getImage(header.gatsbyImageData)!;
 
@@ -18,6 +20,10 @@ const ArticlePreview: React.FC<ArticlePreviewProps> = ({
     <div className={`${className}`}>
       <div className="preview">
         <GatsbyImage image={image} alt={header.description ?? ''} />
+        <Text type="h3" fontWeight="700" className="preview__title">
+          {title}
+        </Text>
+        <Text color="caption">{description.description}</Text>
       </div>
     </div>
   );
