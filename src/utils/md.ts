4a197b7ca5f853ca5977f28c36ca6dd869e426f7
Cyril Troitsky <troickiykirill@gmail.com>
2022-02-06 15:00:43 +0200

    feat: add the ability to create notes

--- src/utils/md.ts
+++
@@ -1,6 +0,0 @@
-import MarkdownIn from 'markdown-it';
-import MarkdownInAttrs from 'markdown-it-attrs';
-
-const md = new MarkdownIn().use(MarkdownInAttrs);
-
-export default md;
---
+++ src/utils/md.tsx
@@ -0,0 +1,7 @@
+import MarkdownIn from 'markdown-it';
+import MarkdownInAttrs from 'markdown-it-attrs';
+import MarkdownBrackets from 'markdown-it-bracketed-spans';
+
+const md = new MarkdownIn().use(MarkdownBrackets).use(MarkdownInAttrs);
+
+export default md;
