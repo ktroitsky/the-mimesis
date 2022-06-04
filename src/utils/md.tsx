94f8ef84781950a67e76e7c89a5dfb83f7310f98
Cyril Troitsky <troickiykirill@gmail.com>
2022-06-04 11:56:30 +0300

    feat: enable html

--- src/utils/md.tsx
+++ src/utils/md.tsx
@@ -2,7 +2,7 @@ import MarkdownIn from 'markdown-it';
 import MarkdownInAttrs from 'markdown-it-attrs';
 import MarkdownBrackets from 'markdown-it-bracketed-spans';
 
-const md = new MarkdownIn({ breaks: true })
+const md = new MarkdownIn({ breaks: true, html: true })
   .use(MarkdownBrackets)
   .use(MarkdownInAttrs);
 
