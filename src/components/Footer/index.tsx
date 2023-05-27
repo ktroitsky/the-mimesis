dc3b94955313be7360daea60cf387f44bf4d8eb2
Kyrylo Troitskyi <troickiykirill@gmail.com>
2023-05-27 11:27:30 +0300

    feat: add socials to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import './style.scss';
+import InstagramIcon from '../../icons/InstagramIcon';
+import YoutubeIcon from '../../icons/YoutubeIcon';
 import Text from '../Text';
+import './style.scss';
 
 const Footer = () => {
   return (
     <div className="footer">
       <Text color="light">&copy; The Mimesis 2023</Text>
+
+      <a href="https://www.youtube.com/@the.mimesis" className="footer__social">
+        <YoutubeIcon />
+      </a>
+
+      <a
+        href="https://www.instagram.com/the.mimesis.ua/"
+        className="footer__social"
+      >
+        <InstagramIcon />
+      </a>
     </div>
   );
 };
